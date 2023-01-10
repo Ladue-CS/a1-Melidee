d97075e0a1b7d3c11a1aca7e97e9732a9777bf8d
Melidee <w.ardecan@gmail.com>
2023-01-10 18:04:02 +0000

    book title to get rating solve

--- 3.7.5/Main.java
+++ 3.7.5/Main.java
@@ -1,16 +1,23 @@
 import java.util.ArrayList;
 
-public class Main
-{
-  public static void main(String[] arg)
-  {
-    String authorToFind = "L. Frank Baum";
+public class Main {
+  public static void main(String[] arg) {
     Library lib = new Library();
     ArrayList<Book> books = lib.getChildrensBooks();
+
+    String authorToFind = "L. Frank Baum";
+    String bookToFind = "Sky Island";
+    Double minimumRating = 0.0;
 
     for (Book book : books) {
         if (book.getAuthor().equals(authorToFind)) {
             System.out.println(book.getTitle());
+        }
+    }
+
+    for (Book book : books) {
+        if (book.getTitle().equals(bookToFind)) {
+            minimumRating = book.getRating();
         }
     }
   }
